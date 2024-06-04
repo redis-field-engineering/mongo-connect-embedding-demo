2895963e58418ac49e972b6f4b8e1ee72d73152e
slorello89 <steve.lorello@redis.com>
2024-06-04 15:48:02 -0400

    adding payload

--- setup_mongo.sh
+++ setup_mongo.sh
@@ -4,4 +4,4 @@ docker run --name mongodb -it --rm --privileged=true \
   --network redis-connect \
   --name redis-mongo-connect \
   --hostname mongo \
-  -p 5432:5432 -d mongo
+  -p 27017:27017 -d mongo
